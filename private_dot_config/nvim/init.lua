2d3aee6ba66f55d281be5552c988871a73837245
shinta@runetale.com <shinta@gx14ac.com>
2026-01-09 10:26:33 +0900

    Switch to Catppuccin Mocha theme

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -9,34 +9,65 @@ vim.opt.termguicolors = true  -- Enable 24-bit RGB colors
 vim.cmd('syntax enable')
 
 --------------------------------------------------------------------
--- Dracula Theme (dracula.nvim - Treesitter supported)
+-- Catppuccin Theme (Treesitter supported)
 --------------------------------------------------------------------
-require("dracula").setup({
-  colors = {
-    bg = "#282A36",
-    fg = "#F8F8F2",
-    selection = "#44475A",
-    comment = "#6272A4",
-    red = "#FF5555",
-    orange = "#FFB86C",
-    yellow = "#F1FA8C",
-    green = "#50FA7B",
-    purple = "#BD93F9",
-    cyan = "#8BE9FD",
-    pink = "#FF79C6",
-    bright_red = "#FF6E6E",
-    bright_green = "#69FF94",
-    bright_yellow = "#FFFFA5",
-    bright_blue = "#D6ACFF",
-    bright_magenta = "#FF92DF",
-    bright_cyan = "#A4FFFF",
-    bright_white = "#FFFFFF",
+require("catppuccin").setup({
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = {
+    light = "latte",
+    dark = "mocha",
   },
-  show_end_of_buffer = true,
-  transparent_bg = false,
-  italic_comment = true,
+  transparent_background = false,
+  show_end_of_buffer = false,
+  term_colors = true,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  no_italic = false,
+  no_bold = false,
+  no_underline = false,
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    telescope = {
+      enabled = true,
+    },
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+    },
+  },
 })
-vim.cmd('colorscheme dracula')
+vim.cmd.colorscheme "catppuccin"
 
 --------------------------------------------------------------------
 -- Treesitter
